41b7f6a8fbe51cbede2c7fbc8c2136f9f54d1569
ThomasB_Mac <thomas.bernard@ensc.fr>
2019-09-13 15:44:17 +0200

    Test app first commit

---
+++ MobileAndroidIOSUnity/Assets/Scripts/BasicShit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicShit : MonoBehaviour
+{
+    public Material m_UiColor;
+
+
+    public void ChangeUIColor()
+    {
+        Debug.Log("Button clicked !");
+        if(m_UiColor != null)
+        {
+            if (m_UiColor.color == Color.red)
+            {
+                m_UiColor.color = Color.blue;
+            }
+            else
+            {
+                m_UiColor.color = Color.red;
+            }
+        }
+        
+    }
+}
